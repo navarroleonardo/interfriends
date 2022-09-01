5b4f16e0eaf4932219b3b94700b3d9a10422eeee
Leonardo Navarro <abreunavarro.leonardo@gmail.com>
2022-08-31 23:03:33 -0300

    chore: commit inicial do projeto spring

---
+++ src/main/java/com/fatec/interfriends/InterfriendsApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.interfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/interfriends/InterfriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.interfriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
