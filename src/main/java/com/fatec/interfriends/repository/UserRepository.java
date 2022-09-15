d78da1abe5c9f2d88b8b9db1c73517c98cd1dbde
Leonardo Navarro <abreunavarro.leonardo@gmail.com>
2022-09-15 00:06:42 -0300

    refact: ajustes no repositório de usuário

--- src/main/java/com/fatec/interfriends/repository/UserRepository.java
+++ src/main/java/com/fatec/interfriends/repository/UserRepository.java
@@ -1,18 +1,13 @@
 package com.fatec.interfriends.repository;
 
-import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.Optional;
-import java.util.OptionalInt;
-
+import com.fatec.interfriends.domain.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
-import com.fatec.interfriends.domain.model.UserModel;
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, Long>{
+public interface UserRepository extends JpaRepository<UserModel, Long> {
 
 	Optional<UserModel> findByEmail(String email);
 
