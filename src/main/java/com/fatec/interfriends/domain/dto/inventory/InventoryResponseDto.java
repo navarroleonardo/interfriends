dce314d9cf2db7db2b2b208ee736a83364baca7e
Leonardo Navarro <abreunavarro.leonardo@gmail.com>
2022-10-14 23:58:38 -0300

    refact: remoção de anotações desnecessárias em Dto de resposta

--- src/main/java/com/fatec/interfriends/domain/dto/inventory/InventoryResponseDto.java
+++ src/main/java/com/fatec/interfriends/domain/dto/inventory/InventoryResponseDto.java
@@ -3,16 +3,11 @@ package com.fatec.interfriends.domain.dto.inventory;
 import com.fatec.interfriends.domain.model.ProductSize;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-
 @Data
 public class InventoryResponseDto {
 
-    @NotNull
     private Long productId;
-    @NotNull
     private Long sizeId;
-    @NotNull
     private Long quantity;
 
     public InventoryResponseDto(ProductSize productSize) {
