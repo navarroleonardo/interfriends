fd22aed0b1c17db677584b08cc10f5058fe0c61e
Leonardo Navarro <abreunavarro.leonardo@gmail.com>
2022-11-30 10:46:36 -0300

    feat: habilitando cors

--- src/main/java/com/fatec/interfriends/config/security/WebSecurityConfig.java
+++ src/main/java/com/fatec/interfriends/config/security/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http/*.cors().and()*/.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/user/login").permitAll()
                 .antMatchers(HttpMethod.POST, "/user/").permitAll()
